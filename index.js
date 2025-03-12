5d6e38edc8f058dfdb1f1bafe54ed82b8463e409
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-12 15:50:38 +0630

    BlogSpace - Add styling

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
     const postArr = data.slice(0, 5);
-    console.log(postArr);
     let html = "";
     for (let post of postArr) {
       html += `

ec5d02dc8fbfe29e12bbfc79469442c7e192d307
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-13 10:35:23 +0630

    Add new post to list of posts

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ newPost.addEventListener("submit", (e) => {
   };
   fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((post) => {
+      blogList.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+        ${blogList.innerHTML}
+      `;
+    });
 });
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")

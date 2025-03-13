f9454e4ea5ab398839c29f26cebf9c4475ac8d27
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-13 10:09:05 +0630

    Form submit event listener

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const blogList = document.getElementById("blog-list");
+const newPost = document.getElementById("new-post");
+
+newPost.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+  console.log(data)
+});
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
